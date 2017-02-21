0e5479dc9476b0c9c460808e3fc61e06b5183a2a
Valentin PERRAUDIN <valentinperraudin@hotmail.fr>
2017-02-21 18:38:02 +0100

    Few modifications

--- DataVisualization/Pages/Market.xaml.cs
+++ DataVisualization/Pages/Market.xaml.cs
@@ -30,21 +30,22 @@ namespace DataVisualization.Pages
             Darky.Visibility = Visibility.Collapsed;
             Bat.Visibility = Visibility.Collapsed;
             Super.Visibility = Visibility.Visible;
+            MyComboBox.
         }
 
         private void EMA_Click(object sender, RoutedEventArgs e)
         {
-
+            Text_block_Tool.Text = "EMA";
         }
 
         private void MACD_Click(object sender, RoutedEventArgs e)
         {
-
+            Text_block_Tool.Text = "MACD";
         }
 
         private void SMA_Click(object sender, RoutedEventArgs e)
         {
-
+            Text_block_Tool.Text = "SMA";
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -60,18 +61,21 @@ namespace DataVisualization.Pages
                 Darky.Visibility = Visibility.Collapsed;
                 Bat.Visibility = Visibility.Visible;
                 Super.Visibility = Visibility.Collapsed;
+                Text_bloc_symbol.Text = "bat";
             }
             else if (item.Content.ToString() == "darky")
             {
                 Darky.Visibility = Visibility.Visible;
                 Bat.Visibility = Visibility.Collapsed;
                 Super.Visibility = Visibility.Collapsed;
+                Text_bloc_symbol.Text = "darky";
             }
             else if (item.Content.ToString() == "super")
             {
                 Darky.Visibility = Visibility.Collapsed;
                 Bat.Visibility = Visibility.Collapsed;
                 Super.Visibility = Visibility.Visible;
+                Text_bloc_symbol.Text = "super";
             }
         }
     }
