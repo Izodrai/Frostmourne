b50bc93ca69464f16675cd6a526613e57866786b
Valentin <vap@capitaldata.fr>
2017-08-04 17:45:32 +0200

    Refactor BidsController with the new common librarie

--- DataAPI/Controllers/BidsController.cs
+++ DataAPI/Controllers/BidsController.cs
@@ -1,15 +1,10 @@
 ﻿using DataAPI.Models;
 using Frostmourne_basics;
+using Frostmourne_basics.Commands;
 using Frostmourne_basics.Dbs;
-using Swashbuckle.Swagger.Annotations;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Configuration;
 using System.Web.Http;
-using xAPI.Commands;
 using xAPI.Sync;
 
 namespace DataAPI.Controllers
@@ -17,120 +12,126 @@ namespace DataAPI.Controllers
     public partial class BidsController : ApiController
     {
         [HttpGet]
-        public Response Update_Symbols(string arg1)
+        public Response Feed_Symbol_From_Last_Insert(string arg1)
         {
             Error err;
             Mysql MyDB = new Mysql();
             SyncAPIConnector Xtb_api_connector = null;
             Configuration configuration = new Configuration();
-            configuration.LoadAPIConfigurationSettings();
+            Data_api_configuration.LoadAPIConfigurationSettings(ref configuration);
 
             err = Tool.InitAll(ref Xtb_api_connector, ref configuration, ref MyDB);
             if (err.IsAnError)
                 return new Response(err, null, null);
 
-            DateTime tNow = DateTime.UtcNow;
-            DateTime tFrom = DateTime.Parse(arg1);
+            List<Symbol> symbol_list = new List<Symbol>();
 
-            List<Bid> bids = new List<Bid>();
-
-            err = Xtb.Retrieve_and_update_data_for_symbols(MyDB, Xtb_api_connector, tNow, tFrom, ref bids, configuration);
+            err = Commands.Load_all_symbols_status(ref Xtb_api_connector, ref configuration, ref MyDB, ref symbol_list);
             if (err.IsAnError)
                 return new Response(err, null, null);
 
-            return new Response(new Error(false, "All non inactive symbols updated, tFrom -> " + tFrom.ToString("yyyy-MM-dd HH:mm:ss") + " -> to -> tNow -> " + tNow.ToString("yyyy-MM-dd HH:mm:ss")), null, null);
+            Symbol s_to_feed = new Symbol();
+
+            foreach (Symbol s in symbol_list)
+            {
+                if (s.Id.ToString() == arg1)
+                {
+                    s_to_feed = s;
+                    break;
+                }
+            }
+
+            if (s_to_feed.Id == 0)
+                return new Response(new Error(false, "This ID doesn't exist or inactive : " + arg1), null, null);
+
+            s_to_feed.Description = "";
+
+            List<Bid> bids_treated = new List<Bid>();
+            err = Commands.Get_from_xtb_stock_values_from_last_insert_for_symbol(ref Xtb_api_connector, ref configuration, ref MyDB, s_to_feed, ref bids_treated);
+            if (err.IsAnError)
+                return new Response(err, null, null);
+
+            return new Response(new Error(false, "Data for symbol " + s_to_feed.Id.ToString() + " (" + s_to_feed.Name + ") feeded"), bids_treated, null);
         }
 
         [HttpGet]
-        public Response Update_Symbol(string arg1, string arg2)
+        public Response Get_Data_For_Symbol(string arg1, string arg2, string arg3)
         {
             Error err;
             Mysql MyDB = new Mysql();
             SyncAPIConnector Xtb_api_connector = null;
             Configuration configuration = new Configuration();
-            configuration.LoadAPIConfigurationSettings();
+            Data_api_configuration.LoadAPIConfigurationSettings(ref configuration);
 
             err = Tool.InitAll(ref Xtb_api_connector, ref configuration, ref MyDB);
             if (err.IsAnError)
                 return new Response(err, null, null);
+
+            List<Symbol> symbol_list = new List<Symbol>();
+
+            err = Commands.Load_all_symbols_status(ref Xtb_api_connector, ref configuration, ref MyDB, ref symbol_list);
+            if (err.IsAnError)
+                return new Response(err, null, null);
+
+            Symbol s_to_load = new Symbol();
+
+            foreach (Symbol s in symbol_list)
+            {
+                if (s.Id.ToString() == arg1)
+                {
+                    s_to_load = s;
+                    break;
+                }
+            }
+
+            if (s_to_load.Id == 0)
+                return new Response(new Error(false, "This ID doesn't exist or inactive : " + arg1), null, null);
             
-            DateTime tNow = DateTime.UtcNow;
-            DateTime tFrom = DateTime.Parse(arg2);
+            DateTime from = new DateTime();
+
+            try
+            {
+                from = DateTime.Parse(arg2);
+            }
+            catch
+            {
+                return new Response(new Error(false, "Bad date format -> " + arg2), null, null);
+            }
+
+            if (from == new DateTime())
+            {
+                return new Response(new Error(false, "Bad date format -> " + arg2), null, null);
+            }
+
+            DateTime to = new DateTime();
+
+            try
+            {
+                to = DateTime.Parse(arg3);
+            }
+            catch
+            {
+                return new Response(new Error(false, "Bad date format -> " + arg3), null, null);
+            }
+
+            if (to == new DateTime())
+            {
+                return new Response(new Error(false, "Bad date format -> " + arg3), null, null);
+            }
+
+            if (from.Date >= to.Date)
+            {
+                return new Response(new Error(false, "from.Date >= to.Date"), null, null);
+            }
 
             List<Bid> bids = new List<Bid>();
 
-            err = Xtb.Retrieve_and_update_data_for_symbol(MyDB, Xtb_api_connector, new Symbol(0, arg1, ""), tNow, tFrom, ref bids, configuration);
+            err = Commands.Get_from_db_stock_values_between_two_date_for_symbol(ref Xtb_api_connector, ref configuration, ref MyDB, s_to_load, from, to, ref bids);
             if (err.IsAnError)
                 return new Response(err, null, null);
 
-            return new Response(new Error(false, "Data for symbol " + arg1 + " updated between " + tFrom.ToString("yyyy-MM-dd HH:mm:ss") + " and " + tNow.ToString("yyyy-MM-dd HH:mm:ss")), null, null);
-        }
+            return new Response(new Error(false, "Data for symbol " + s_to_load.Id.ToString() + " (" + s_to_load.Name + ") loaded"), bids, null);
 
-        [HttpGet]
-        public Response Get_Symbol(string arg1, string arg2)
-        {
-            Error err;
-            Mysql MyDB = new Mysql();
-            Configuration configuration = new Configuration();
-            configuration.LoadAPIConfigurationSettings();
-
-            err = Tool.InitMyDb(ref configuration, ref MyDB);
-            if (err.IsAnError)
-                return new Response(err, null, null);
-
-            DateTime tNow = DateTime.UtcNow;
-            DateTime tFrom = DateTime.Parse(arg2);
-            List<Bid> bids = new List<Bid>();
-
-            err = Xtb.Load_bids_symbol(MyDB, new Symbol(0, arg1, ""), tNow, tFrom, ref bids);
-            if (err.IsAnError)
-                return new Response(err, null, null);
-
-            return new Response(new Error(false, "Data ok"), bids, null);
-        }
-
-        [HttpGet]
-        public Response Get_Symbols(string arg1)
-        {
-            Error err;
-            Mysql MyDB = new Mysql();
-            Configuration configuration = new Configuration();
-            configuration.LoadAPIConfigurationSettings();
-
-            err = Tool.InitMyDb(ref configuration, ref MyDB);
-            if (err.IsAnError)
-                return new Response(err, null, null);
-
-            DateTime tNow = DateTime.UtcNow;
-            DateTime tFrom = DateTime.Parse(arg1);
-            List<Bid> bids = new List<Bid>();
-
-            err = Xtb.Load_bids_symbols(MyDB, tNow, tFrom, ref bids);
-            if (err.IsAnError)
-                return new Response(err, null, null);
-
-            return new Response(new Error(false, "Data ok"), bids, null);
-        }
-
-        [HttpGet]
-        public Response Get_Symbols_To_Retrieve()
-        {
-            Error err;
-            Mysql MyDB = new Mysql();
-            Configuration configuration = new Configuration();
-            configuration.LoadAPIConfigurationSettings();
-
-            err = Tool.InitMyDb(ref configuration, ref MyDB);
-            if (err.IsAnError)
-                return new Response(err, null, null);
-
-            List<Symbol> symbols = new List<Symbol>();
-
-            err = Xtb.Load_symbols_to_retrieve(MyDB, ref symbols);
-            if (err.IsAnError)
-                return new Response(err, null, null);
-
-            return new Response(new Error(false, "Data ok"), null, symbols);
         }
     }
 }
---
+++ DataAPI/Data_api_configuration.cs
@@ -0,0 +1,137 @@
+﻿using Frostmourne_basics;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace DataAPI
+{
+    public class Data_api_configuration
+    {
+        public static Error LoadAPIConfigurationSettings(ref Frostmourne_basics.Configuration Config)
+        {
+            //////////////////////////////////////////////////
+            // Load de l'environnement
+            //////////////////////////////////////////////////
+
+            try
+            {
+                Config.Environnement = WebConfigurationManager.AppSettings["Environnement"];
+            }
+            catch (ConfigurationErrorsException e)
+            {
+                return new Error(true, "Error reading app settings xtb_login : " + e.Message);
+            }
+
+            if (Config.Environnement == "prod")
+            {
+                Config.Prod = true;
+            }
+            else
+            {
+                Config.Environnement = "dev";
+            }
+
+            //////////////////////////////////////////////////
+            // Load de l'id de l'utilisateur
+            //////////////////////////////////////////////////
+
+            try
+            {
+                Config.Xtb_login = WebConfigurationManager.AppSettings["Xtb_login"];
+            }
+            catch (ConfigurationErrorsException e)
+            {
+                return new Error(true, "Error reading app settings xtb_login : " + e.Message);
+            }
+
+            //////////////////////////////////////////////////
+            // Load du mot de passe de l'utilisateur
+            //////////////////////////////////////////////////
+
+            try
+            {
+                Config.Xtb_pwd = WebConfigurationManager.AppSettings["xtb_pwd"];
+            }
+            catch (ConfigurationErrorsException e)
+            {
+                return new Error(true, "Error reading app settings UserPwd : " + e.Message);
+            }
+
+            //////////////////////////////////////////////////
+            // Load du type de connexion aux serveurs (demo ou real uniquement)
+            //////////////////////////////////////////////////
+
+            try
+            {
+                Config.Set_server(WebConfigurationManager.AppSettings["Xtb_Server"]);
+            }
+            catch (ConfigurationErrorsException e)
+            {
+                return new Error(true, "Error reading app settings Server : " + e.Message);
+            }
+
+            //////////////////////////////////////////////////
+            // Load de l'host et mdp mysql suivant l'environnement
+            //////////////////////////////////////////////////
+
+            try
+            {
+                Config.Mysql_host = WebConfigurationManager.AppSettings["Mysql_host"];
+            }
+            catch (ConfigurationErrorsException e)
+            {
+                return new Error(true, "Error reading app settings Mysql_host : " + e.Message);
+            }
+            try
+            {
+                Config.Mysql_pwd = WebConfigurationManager.AppSettings["Mysql_pwd"];
+            }
+            catch (ConfigurationErrorsException e)
+            {
+                return new Error(true, "Error reading app settings Mysql_pwd : " + e.Message);
+            }
+            try
+            {
+                Config.Mysql_port = WebConfigurationManager.AppSettings["Mysql_port"];
+            }
+            catch (ConfigurationErrorsException e)
+            {
+                return new Error(true, "Error reading app settings Mysql_port : " + e.Message);
+            }
+
+            //////////////////////////////////////////////////
+            // Load du login mysql
+            //////////////////////////////////////////////////
+
+            try
+            {
+                Config.Mysql_login = WebConfigurationManager.AppSettings["Mysql_login"];
+            }
+            catch (ConfigurationErrorsException e)
+            {
+                return new Error(true, "Error reading app settings Mysql_login : " + e.Message);
+            }
+
+            //////////////////////////////////////////////////
+            // Load de la base de données
+            //////////////////////////////////////////////////
+
+            try
+            {
+                Config.Mysql_database = WebConfigurationManager.AppSettings["Mysql_database"];
+            }
+            catch (ConfigurationErrorsException e)
+            {
+                return new Error(true, "Error reading app settings Mysql_database : " + e.Message);
+            }
+
+            Log.GreenInfo("Configuration loaded");
+            Log.JumpLine();
+
+            return new Error(false, "Web setting loaded!");
+        }
+    }
+}
