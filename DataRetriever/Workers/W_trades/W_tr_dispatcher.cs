4ca7cf4d866d591379e06a002015bd641ba5c341
Valentin <vap@capitaldata.fr>
2017-08-22 15:43:57 +0200

    few updates for close a trade

--- DataRetriever/Workers/W_trades/W_tr_dispatcher.cs
+++ DataRetriever/Workers/W_trades/W_tr_dispatcher.cs
@@ -25,6 +25,8 @@ namespace DataRetriever.Workers.W_trades
             Log.MagentaInfo("Trade Menu ");
             Log.YellowInfo("What do you want to do ?");
             Log.WhiteInfo("(1) -> Open a trade");
+            Log.CyanInfo("(2) -> See opened trades");
+            Log.WhiteInfo("(3) -> Close a trade");
             Log.WhiteInfo("(0) -> Return To Main Menu");
         }
         public static Error Dispatch_choice(string _choice, ref SyncAPIConnector Xtb_api_connector, ref Configuration configuration, ref Mysql MyDB)
@@ -37,6 +39,17 @@ namespace DataRetriever.Workers.W_trades
             {
                 case "1":
                     err = OpenTrade(ref Xtb_api_connector, ref configuration, ref MyDB);
+                    if (err.IsAnError)
+                        return err;
+                    break;
+                case "2":
+                    List<Trade> trades = new List<Trade>();
+                    err = OpenedTrades(ref Xtb_api_connector, ref configuration, ref MyDB, ref trades, "Opened Trade menu");
+                    if (err.IsAnError)
+                        return err;
+                    break;
+                case "3":
+                    err = CloseTrade(ref Xtb_api_connector, ref configuration, ref MyDB);
                     if (err.IsAnError)
                         return err;
                     break;
@@ -98,19 +111,147 @@ namespace DataRetriever.Workers.W_trades
                 return new Error(false, "");
             }
 
+            Trade trade = new Trade();
+
             Log.WhiteInfo("Which CMD choice ? (0 -> buy, 1 -> sell)");
 
-            //int cmd = Convert.ToInt32(Console.ReadLine());
-            int cmd = 0;
+            //trade.Trade_type = Convert.ToInt32(Console.ReadLine());
+            trade.Trade_type = 0;
 
             Log.WhiteInfo("Which volume choice ? (x,y)");
 
-            //double volume = Convert.ToDouble(Console.ReadLine());
-            double volume = 0.1;
+            //trade.Volume = Convert.ToDouble(Console.ReadLine());
+            trade.Volume = 0.1;
 
-            err = Commands.Open_trade_xtb(Xtb_api_connector, ref configuration, ref MyDB, s_to_check, cmd, volume);
+            trade.Opened_reason = "test_opened";
+
+            err = Commands.Open_trade_xtb(ref Xtb_api_connector, ref configuration, ref MyDB, s_to_check, ref trade);
             if (err.IsAnError)
                 return err;
+
+            Log.JumpLine();
+            Log.Info("A trade has been opened :");
+            Log.Info("Id in db : " + trade.Id);
+            Log.Info("Id in xtb : " + trade.Xtb_order_id_2);
+            Log.Info("Opened_at : " + trade.Opened_at.ToString("yyyy-MM-dd HH:mm:ss"));
+            Log.Info("Opened_price : " + trade.Opened_price.ToString());
+            Log.Info("Stop_loss : " + trade.Stop_loss.ToString());
+            Log.Info("Volume : " + trade.Volume.ToString());
+            Log.Info("Opened_reason : " + trade.Opened_reason);
+            if (trade.Trade_type == 0)
+                Log.Info("Action : buy");
+            else
+                Log.Info("Action : sell");
+            
+            return err;
+        }
+
+        public static Error OpenedTrades(ref SyncAPIConnector Xtb_api_connector, ref Configuration configuration, ref Mysql MyDB, ref List<Trade> _trades, string msg)
+        {
+            Error err = new Error();
+            
+            Log.JumpLine();
+            Log.JumpLine();
+            Log.Info("########################");
+            Log.JumpLine();
+            Log.JumpLine();
+            
+            Log.MagentaInfo(msg);
+
+            err = Commands.Get_open_trades_from_xtb(ref Xtb_api_connector, ref configuration, ref MyDB, ref _trades);
+            if (err.IsAnError)
+                return err;
+
+            Log.JumpLine();
+
+            if (_trades.Count == 0)
+            {
+                Log.YellowInfo("no trades to display");
+                return new Error(false, "no trades to display");
+            }
+
+            Log.YellowInfo("List of opened trades: ");
+            Log.WhiteInfo("| Id | Symbol_id (Name) | Xtb_order_id_1 | Xtb_order_id_2 | Opened_at | Opened_value | Stop_loss | Opened_reason | Action | Profit | ");
+
+            foreach (Trade t in _trades)
+            {
+                string action;
+                if (t.Trade_type == 0)
+                    action = "buy";
+                else
+                    action = "sell";
+
+                Log.Info("| " + t.Id + " | " + t.Symbol.Id.ToString() + " ( " + t.Symbol.Name + ") | " + t.Xtb_order_id_1 + " | " + t.Xtb_order_id_2 + " | " + t.Opened_at + " | " + t.Opened_price + " | " + t.Stop_loss + " | " + t.Opened_reason + " | " + action + " | " + t.Profit + " |");
+            }
+
+            return err;
+        }
+
+        public static Error CloseTrade(ref SyncAPIConnector Xtb_api_connector, ref Configuration configuration, ref Mysql MyDB)
+        {
+            Error err = new Error();
+            List<Trade> trades = new List<Trade>();
+
+            err = OpenedTrades(ref Xtb_api_connector, ref configuration, ref MyDB, ref trades, "Close Trade Menu");
+            if (err.IsAnError)
+                return err;
+            
+            Log.Info("| 0 | Exit | Abort |");
+            Log.JumpLine();
+
+            Log.WhiteInfo("On which trade do you want close ? (Write the db ID)");
+
+            string choice = Console.ReadLine();
+            if (choice == "0")
+            {
+                Log.WhiteInfo("Close Trade aborted");
+                return new Error(false, "");
+            }
+
+            Trade trade_to_close = new Trade();
+
+            foreach (Trade t in trades)
+            {
+                if (t.Id.ToString() == choice)
+                {
+                    trade_to_close = t;
+                    break;
+                }
+            }
+
+            if (trade_to_close.Id == 0)
+            {
+                Log.Error("This ID doesn't exist : " + choice);
+                return new Error(false, "");
+            }
+
+            trade_to_close.Closed_reason = "test_close";
+
+            err = Commands.Close_trade_xtb(ref Xtb_api_connector, ref configuration, ref MyDB, ref trade_to_close);
+            if (err.IsAnError)
+                return err;
+
+            Log.JumpLine();
+            Log.Info("A trade has been closed :");
+            Log.Info("Id in db : " + trade_to_close.Id);
+            Log.Info("Id in xtb 1: " + trade_to_close.Xtb_order_id_1);
+            Log.Info("Id in xtb 2: " + trade_to_close.Xtb_order_id_2);
+            Log.Info("Stop_loss : " + trade_to_close.Stop_loss.ToString());
+            Log.Info("Volume : " + trade_to_close.Volume.ToString());
+            if (trade_to_close.Trade_type == 0)
+                Log.Info("Action : buy");
+            else
+                Log.Info("Action : sell");
+            Log.JumpLine();
+            Log.Info("Profit : " + trade_to_close.Profit.ToString());
+            Log.JumpLine();
+            Log.Info("Opened_at : " + trade_to_close.Opened_at.ToString("yyyy-MM-dd HH:mm:ss"));
+            Log.Info("Opened_price : " + trade_to_close.Opened_price.ToString());
+            Log.Info("Opened_reason : " + trade_to_close.Opened_reason);
+            Log.JumpLine();
+            Log.Info("Closed_at : " + trade_to_close.Closed_at.ToString("yyyy-MM-dd HH:mm:ss"));
+            Log.Info("Closed_price : " + trade_to_close.Closed_price.ToString());
+            Log.Info("Closed_reason : " + trade_to_close.Closed_reason);
             
             return err;
         }
